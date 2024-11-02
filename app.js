266675b7e343921e9233f8ae75092ac6bea76320
Shubham Kumar <33327989+shubhr86@users.noreply.github.com>
2024-11-02 19:23:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const issueRoutes = require('./routes/issueroutes');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/issue-tracker', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://freemovie9com:DV0AyqOqo5QmEt9b@cluster0.k6ddc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(error => console.error('MongoDB connection error:', error));
 
 // Middleware
 app.set('view engine', 'ejs');
